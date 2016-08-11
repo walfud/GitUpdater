38afd2638eb1d13e039e0345f307141a7d386b6e
walfud <walfud@aliyun.com>
2016-08-11 17:08:13 +0800

    pull submodules

--- index.js
+++ index.js
@@ -1,4 +1,5 @@
 import http from 'http';
+import series from 'async/series';
 import {
     spawn
 } from 'child_process';
@@ -9,16 +10,43 @@ const server = http.createServer((req, res) => {
     const url = req.url;
     let body = [];
 
-    req.on('error', (err) => {
-        console.error(err);
-    }).on('data', (data) => {
-        body.push(data);
-    }).on('end', () => {
-        spawn('git', ['pull']).stdout.on('data', data => console.log(data.toString()));
-    });
-
-    // Response
-    res.statusCode = 200;
-    res.end();
+    console.log('pulling...');
+    series([
+            callback => invoke('git', ['pull'], callback),
+            callback => invoke('git', ['submodule', 'update'], callback),
+            callback => invoke('git', ['submodule', 'foreach', 'git', 'checkout', 'master'], callback),
+            callback => invoke('git', ['submodule', 'foreach', 'git', 'pull'], callback),
+        ],
+        (err, result) => {
+            // Response
+            res.statusCode = 200;
+            if (err) {
+                res.write(err);
+                console.error(err);
+            } else {
+                res.write(result.join('\n'));
+                console.log('done');
+            }
+            res.end();
+        });
 });
 server.listen(80);
+
+function invoke(cmd, args, callback) {
+    const result = spawn(cmd, args);
+    console.log(result.spawnargs.join(" "));
+
+    let stdout = [];
+    let stderr = [];
+    result.stdout.on('data', data => {
+        stdout.push(data);
+        console.log(data.toString());
+    });
+    result.stderr.on('data', data => {
+        stderr.push(data);
+        console.error(data.toString());
+    });
+    result.on('exit', () => {
+        callback(stderr.toString(), stdout.toString());
+    });
+}
